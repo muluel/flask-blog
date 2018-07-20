9bf8469c67da770d610b831d24e814b2cff67375
Muhammed uluel <muhammeduluel@outlook.com>
2018-07-20 16:16:42 +0300

    some features and register models added

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,12 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from flask_login import LoginManager
+
 app = Flask(__name__)
 app.config.from_object(Config)
+login = LoginManager(app)
+login.login_view = 'login'
 
 # Database setting
 db = SQLAlchemy(app)
--- app/models.py
+++ app/models.py
@@ -2,9 +2,11 @@
 from datetime import datetime
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
 # user model
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -19,7 +21,13 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-        
+
+# User Loader Function
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
 # Post Model
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
