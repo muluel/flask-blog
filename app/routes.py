60b32ffcf758f9790eda184fb71d065e28288267
Muhammed uluel <muhammeduluel@outlook.com>
2018-07-21 17:27:06 +0300

    write post added

--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,28 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
-from app.models import User
+from app.models import User, Post
 from datetime import datetime
 
 # Url setting
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET','POST'])
+@app.route('/index', methods=['GET','POST'])
 @login_required
 
 # index view
 def index():
-    user={'username':'John', 'username':'Susan'}
-    posts=[
-        {
-            'author':{'username':'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author':{'username':'Susan'},
-            'body': 'The Avangers movie was so cool!'
-        }
-    ]
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is sended.')
+        return redirect(url_for('index'))
+    posts = current_user.followed_posts().all()
     # view leri render ederek son hallerinin ne olmasini soyler
-    return render_template('index.html', title='Home', posts=posts)
+    return render_template('index.html', title='Home Page', form=form, posts=posts)
 
 #login sayfasi icin url
 @app.route('/login', methods=['GET','POST'])
@@ -137,3 +134,10 @@ def unfollow(username):
     db.session.commit()
     flash('You are not following {}.'.format(username))
     return redirect(url_for('user', username=username))
+
+@app.route('/explore')
+@login_required
+
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
